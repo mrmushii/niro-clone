4177e4086022ec3935989a7dabf0672d92ef723c
Mushfiq <mushfiq2808@gmail.com>
2025-10-01 12:05:55 +0600

    completed organization

--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -1,12 +1,13 @@
 "use client"
 
 import { UserButton } from "@clerk/nextjs"
+import SearchInput from "./SearchInput"
 
 const Navbar = () => {
   return (
     <div className="flex items-center gap-x-4 p-5 bg-green-500">
       <div className="hidden lg:flex lg:flex-1">
-        {/* todo: add search */}
+        <SearchInput/>
       </div>
       <UserButton/>
     </div>
